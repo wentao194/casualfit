baf4269e224203280a0ffff6f5da1e9adfbfc6d6
Wesley <35483683+wentao194@users.noreply.github.com>
2019-04-15 15:20:39 -0700

    Add files via upload

---
+++ my.js
@@ -0,0 +1,15 @@
+function fadeOutEffect() {
+    var fadeTarget = document.getElementById("target");
+    var fadeEffect = setInterval(function () {
+        if (!fadeTarget.style.opacity) {
+            fadeTarget.style.opacity = 1;
+        }
+        if (fadeTarget.style.opacity > 0) {
+            fadeTarget.style.opacity -= 0.1;
+        } else {
+            clearInterval(fadeEffect);
+        }
+    }, 200);
+}
+
+document.getElementById("target").addEventListener('click', fadeOutEffect);
